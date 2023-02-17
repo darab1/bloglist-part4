d2dae5279c1855eca9645ce5bc54e1c06310e4b0
Dimitris <dimarabatzis@outlook.com>
2023-02-17 17:09:37 +0200

    Finished 4.1 Blog list, step1

---
+++ index.js
@@ -0,0 +1,61 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.set('strictQuery', false)
+
+const mongoUrl ='mongodb+srv://fullstack:fullstack@cluster0.krnbxxv.mongodb.net/bloglistApp?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const body = request.body
+  
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0
+  })
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => console.log('error in creating a new blog: ', error))
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
