f0b0f2fe894146ff0b41bec6e392c9b4951dcc7d
Dimitris <dimarabatzis@outlook.com>
2023-03-06 13:35:18 +0200

    Finished 4.19: bloglist expansion, step7

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blogModel')
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
 // GET ALL BLOGS
 blogRouter.get('/', async (req, res) => {
@@ -11,18 +12,41 @@ blogRouter.get('/', async (req, res) => {
   res.status(200).json(blogs)
 })
 
+// GET TOKEN
+const getToken = request => {
+  const authorization = request.get('authorization')
+  console.log('authorization', authorization)
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    console.log(`authorization.split(' ')[1]: `, authorization.split(' ')[1])
+    return authorization.split(' ')[1]
+  }
+  return null
+}
+
 // CREATE A NEW BLOG
 blogRouter.post('/', async (req, res) => {
   const body = req.body
 
-  const user = await User.findById(body.userId)
+  const token = getToken(req)
+  console.log('token: ', token)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  console.log('decoded token: ', decodedToken)
+
+  if (!decodedToken.id) {
+    return res.status(401).json({
+      error: 'invalid token'
+    })
+  }
+  
+  const user = await User.findById(decodedToken.id)
   
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: user.id
+    user: user._id
   })
 
   const savedBlog = await blog.save()
